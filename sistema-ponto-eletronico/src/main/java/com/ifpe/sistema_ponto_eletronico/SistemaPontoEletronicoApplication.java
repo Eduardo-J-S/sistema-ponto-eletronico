35c8d6770d9f417492a6ac7b4166dcd21295b849
Eduardo-J-S <eduardojose59@hotmail.com>
2024-07-24 21:47:16 -0300

    feat: add project and create model

---
+++ sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/SistemaPontoEletronicoApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.sistema_ponto_eletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPontoEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPontoEletronicoApplication.class, args);
+	}
+
+}
---
+++ sistema-ponto-eletronico/src/test/java/com/ifpe/sistema_ponto_eletronico/SistemaPontoEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpe.sistema_ponto_eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPontoEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
