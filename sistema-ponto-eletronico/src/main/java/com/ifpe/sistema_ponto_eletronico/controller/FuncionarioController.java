e36aa456f4e6f323417309a5a209bb4ad94828b6
Eduardo-J-S <eduardojose59@hotmail.com>
2024-08-02 16:02:14 -0300

    add: controller Funcionario

---
+++ sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/controller/FuncionarioController.java
@@ -0,0 +1,49 @@
+package com.ifpe.sistema_ponto_eletronico.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ifpe.sistema_ponto_eletronico.dto.FuncionarioDTO;
+import com.ifpe.sistema_ponto_eletronico.service.FuncionarioService;
+
+@RestController
+@RequestMapping("api/funcionario/v1")
+public class FuncionarioController {
+    
+    @Autowired
+    FuncionarioService funcionarioService;
+
+    @GetMapping("/{id}")
+    public FuncionarioDTO findById(@PathVariable Long id){
+        return funcionarioService.findById(id);
+    }
+
+    @GetMapping()
+    public List<FuncionarioDTO> findAll(){
+        return funcionarioService.findAll();
+    }
+
+    @PostMapping()
+    public FuncionarioDTO create(@RequestBody FuncionarioDTO dto){
+        return funcionarioService.create(dto);
+    }
+
+    @PutMapping()
+    public FuncionarioDTO update(@RequestBody FuncionarioDTO dto){
+        return funcionarioService.update(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        funcionarioService.delete(id);
+    }
+}
