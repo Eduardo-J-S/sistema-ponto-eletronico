7b65babd380971332e4389fdd4a540949a16403e
adonaiviedondon <vieiraadonai@gmail.com>
2024-08-12 21:41:22 -0300

    testes admin

--- sistema-ponto-eletronico/src/test/java/com/ifpe/sistema_ponto_eletronico/model/AdminTest.java
+++ sistema-ponto-eletronico/src/test/java/com/ifpe/sistema_ponto_eletronico/model/AdminTest.java
@@ -1,5 +1,48 @@
 package com.ifpe.sistema_ponto_eletronico.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AdminTest {
-    
+    private Admin admin;
+    private Empresa empresa;
+
+    @BeforeEach
+    void setUp(){
+        empresa = new Empresa();
+        empresa.setNomeEmpresa("Empresa fullstack ponto");
+
+        admin = new Admin();
+        admin.setIdAdmin(1L);
+        admin.setNomeAdmin("eduardo");
+        admin.setSenhaAdmin("52678");
+        admin.setDataAlteracao(LocalDateTime.now());
+        admin.setUsuarioAdmin("eduardo");
+        admin.setMatricula("2022ads4537");
+        admin.setEmpresa(empresa);
+    }
+
+    @Test
+    void testGettersAndSetters() {
+        assertEquals(1L, admin.getIdAdmin());
+        assertEquals("eduardo", admin.getNomeAdmin());
+        assertEquals("52678", admin.getSenhaAdmin());
+        assertNotNull(admin.getDataAlteracao());
+        assertEquals("eduardo", admin.getUsuarioAdmin());
+        assertEquals("2022ads4537", admin.getMatricula());
+        assertEquals(empresa, admin.getEmpresa());
+    }
+
+    @Test
+    void testEqualsAndHashCode() {
+        Admin admin2 = new Admin();
+        admin2.setIdAdmin(1L);
+
+        assertEquals(admin, admin2);
+        assertEquals(admin.hashCode(), admin2.hashCode());
+    }
 }
