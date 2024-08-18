53ce309b9c30d7dd8526369bb56631eabc211b2b
Eduardo-J-S <eduardojose59@hotmail.com>
2024-08-17 21:08:56 -0300

    tirando import sem uso

--- sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/repository/AdminRepository.java
+++ sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/repository/AdminRepository.java
@@ -1,7 +1,5 @@
 package com.ifpe.sistema_ponto_eletronico.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ifpe.sistema_ponto_eletronico.model.Admin;
