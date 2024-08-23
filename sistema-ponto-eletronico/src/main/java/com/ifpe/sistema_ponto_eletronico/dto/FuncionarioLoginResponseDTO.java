0e64aaf7afbb9e45b5eae97972e541cdcbc4dd5e
Eduardo-J-S <eduardojose59@hotmail.com>
2024-08-23 17:37:12 -0300

    add id reponse login and register

--- sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/dto/FuncionarioLoginResponseDTO.java
+++ sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/dto/FuncionarioLoginResponseDTO.java
@@ -1,4 +1,4 @@
 package com.ifpe.sistema_ponto_eletronico.dto;
 
 
-public record FuncionarioLoginResponseDTO(String nome, String matricula, String role, String token){}
+public record FuncionarioLoginResponseDTO(Long id, String nome, String matricula, String role, String token){}
