857375f57abf149bcd25ac43d3ad0364f52f79f8
Eduardo-J-S <eduardojose59@hotmail.com>
2024-08-25 12:13:30 -0300

    mais resposta para o user

--- sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/dto/FuncionarioLoginResponseDTO.java
+++ sistema-ponto-eletronico/src/main/java/com/ifpe/sistema_ponto_eletronico/dto/FuncionarioLoginResponseDTO.java
@@ -1,4 +1,4 @@
 package com.ifpe.sistema_ponto_eletronico.dto;
 
 
-public record FuncionarioLoginResponseDTO(Long id, String nome, String matricula, String role, Long empresaID, String token){}
+public record FuncionarioLoginResponseDTO(Long id, String nome, String matricula, String cpf, String status, String role, Long empresaID, String token){}
